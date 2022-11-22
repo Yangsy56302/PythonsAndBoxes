38c6e49faed9a3326e7c9301d1325c79926f15b9
Yangsy56302 <yangsy56302@163.com>
2022-11-22 09:23:06 +0800

    Jump, gravity, EXE and more...

---
+++ exe.bat
@@ -0,0 +1,4 @@
+pyinstaller --onefile --console --distpath . --workpath ./temp/build --icon main.ico --name main.exe main.py
+rd /S /Q temp
+del main.exe.spec
+pause
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def hexadecimal(_number: int) -> str:
 
 class Settings:
     scale = 1
+    gravity = 0.0625
     def __init__(self) -> None:
         pass
 settings = Settings()
@@ -20,6 +21,39 @@ pygame.init()
 window = pygame.display.set_mode((1024, 768))
 pygame.display.set_caption("PyBox")
 window.fill((0, 255, 0))
+
+
+def get_key_states(_events, _states: dict = {}) -> dict:
+    states = _states
+    for event in _events:
+        if event.type == pygame.KEYDOWN:
+            states[event.key] = "down"
+        if event.type == pygame.KEYUP:
+            states[event.key] = "up"
+    for state in states:
+        if "_2" not in states[state]:
+            states[state] += "_2"
+    return states
+
+
+def key_is_down(_states: dict, _key) -> bool:
+    if _key in _states:
+        if "down" in _states[_key]:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def key_is_up(_states: dict, _key) -> bool:
+    if _key in _states:
+        if "up" in _states[_key]:
+            return True
+        else:
+            return False
+    else:
+        return False
 
 
 class Data:
@@ -38,11 +72,14 @@ data = Data()
 
 class Assets:
     tile_images = None
+    mob_images = None
     def load(self) -> None:
         self.tile_images = {}
-        print(data.tile_data)
+        self.mob_images = {}
         for id in data.tile_data.keys():
             self.tile_images[id] = pygame.image.load(".\\assets\\images\\tiles\\" + id + ".png").convert_alpha()
+        for id in data.mob_data.keys():
+            self.mob_images[id] = pygame.image.load(".\\assets\\images\\mobs\\" + id + ".png").convert_alpha()
     def __init__(self) -> None:
         self.load()
 assets = Assets()
@@ -112,18 +149,85 @@ class World:
                     self.map[x][y] = Tile("grassy_soil")
         self.player = Mob("player")
         self.player.state["x"] = 999.0
-        self.player.state["y"] = float(terrain[999])
-    def tick(self, _events) -> None:
-        for event in _events:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    self.player.state["y"] += 0.25
-                if event.key == pygame.K_s:
-                    self.player.state["y"] -= 0.25
-                if event.key == pygame.K_a:
-                    self.player.state["x"] -= 0.25
-                if event.key == pygame.K_d:
-                    self.player.state["x"] += 0.25
+        self.player.state["y"] = float(terrain[999] + 1)
+        self.player.state["mx"] = 0.0
+        self.player.state["my"] = 0.0
+    def mob_on_ground(self, _mob) -> bool:
+        block_coordinate = [[int(_mob.state["x"] + 0.03125), int(_mob.state["y"] - 0.03125)],
+                            [int(_mob.state["x"] + 0.96875), int(_mob.state["y"] - 0.03125)]]
+        for coordinate in block_coordinate:
+            if self.valid_coordinate(coordinate[0], coordinate[1]):
+                if "mob_transparent" not in data.tile_data[self.map[coordinate[0]][coordinate[1]].id]["tag"]:
+                    return True
+        return False
+    def move(self, _mob) -> Mob:
+        if _mob.state["mx"] == 0 and _mob.state["my"] == 0:
+            return _mob
+        max_move = int(max(abs(_mob.state["mx"]), abs(_mob.state["my"])) * 16)
+        list_x = [0 for x in range(max_move + 1)]
+        if _mob.state["mx"] != 0:
+            list_x = [int(x * _mob.state["mx"] * 16 / max_move) / 16 for x in range(max_move + 1)]
+        list_y = [0 for y in range(max_move + 1)]
+        if _mob.state["my"] != 0:
+            list_y = [int(y * _mob.state["my"] * 16 / max_move) / 16 for y in range(max_move + 1)]
+        for i in range(max_move + 1):
+            block_coordinate = [[int(_mob.state["x"] + list_x[i] + 0.03125), int(_mob.state["y"] + list_y[i] + 0.03125)],
+                                [int(_mob.state["x"] + list_x[i] + 0.03125), int(_mob.state["y"] + list_y[i] + 0.96875)],
+                                [int(_mob.state["x"] + list_x[i] + 0.96875), int(_mob.state["y"] + list_y[i] + 0.03125)],
+                                [int(_mob.state["x"] + list_x[i] + 0.96875), int(_mob.state["y"] + list_y[i] + 0.96875)]]
+            for j in range(len(block_coordinate)):
+                if not self.valid_coordinate(block_coordinate[j][0], block_coordinate[j][1]):
+                    _mob.state["x"] = 0.0
+                    _mob.state["y"] = 0.0
+                    _mob.state["mx"] = 0.0
+                    _mob.state["my"] = 0.0
+                    return _mob
+                if "mob_transparent" not in data.tile_data[self.map[block_coordinate[j][0]][block_coordinate[j][1]].id]["tag"]:
+                    if _mob.state["mx"] > 0:
+                        block_coordinate = [[int(_mob.state["x"] + list_x[i - 1] + 1.03125), int(_mob.state["y"] + list_y[i - 1] + 0.03125)],
+                                            [int(_mob.state["x"] + list_x[i - 1] + 1.03125), int(_mob.state["y"] + list_y[i - 1] + 0.96875)]]
+                        for coordinate in block_coordinate:
+                            if self.valid_coordinate(coordinate[0], coordinate[1]):
+                                if "mob_transparent" not in data.tile_data[self.map[coordinate[0]][coordinate[1]].id]["tag"]:
+                                    _mob.state["mx"] = 0.0
+                    else:
+                        block_coordinate = [[int(_mob.state["x"] + list_x[i - 1] - 0.03125), int(_mob.state["y"] + list_y[i - 1] + 0.03125)],
+                                            [int(_mob.state["x"] + list_x[i - 1] - 0.03125), int(_mob.state["y"] + list_y[i - 1] + 0.96875)]]
+                        for coordinate in block_coordinate:
+                            if self.valid_coordinate(coordinate[0], coordinate[1]):
+                                if "mob_transparent" not in data.tile_data[self.map[coordinate[0]][coordinate[1]].id]["tag"]:
+                                    _mob.state["mx"] = 0.0
+                    if _mob.state["my"] > 0:
+                        block_coordinate = [[int(_mob.state["x"] + list_x[i - 1] + 0.03125), int(_mob.state["y"] + list_y[i - 1] + 1.03125)],
+                                            [int(_mob.state["x"] + list_x[i - 1] + 0.96875), int(_mob.state["y"] + list_y[i - 1] + 1.03125)]]
+                        for coordinate in block_coordinate:
+                            if self.valid_coordinate(coordinate[0], coordinate[1]):
+                                if "mob_transparent" not in data.tile_data[self.map[coordinate[0]][coordinate[1]].id]["tag"]:
+                                    _mob.state["my"] = 0.0
+                    else:
+                        block_coordinate = [[int(_mob.state["x"] + list_x[i - 1] + 0.03125), int(_mob.state["y"] + list_y[i - 1] - 0.03125)],
+                                            [int(_mob.state["x"] + list_x[i - 1] + 0.96875), int(_mob.state["y"] + list_y[i - 1] - 0.03125)]]
+                        for coordinate in block_coordinate:
+                            if self.valid_coordinate(coordinate[0], coordinate[1]):
+                                if "mob_transparent" not in data.tile_data[self.map[coordinate[0]][coordinate[1]].id]["tag"]:
+                                    _mob.state["my"] = 0.0
+                    _mob.state["x"] += list_x[i - 1]
+                    _mob.state["y"] += list_y[i - 1]
+                    return _mob
+        _mob.state["x"] += list_x[-1]
+        _mob.state["y"] += list_y[-1]
+        return _mob
+    def tick(self, _states) -> None:
+        self.player.state["mx"] = 0.0
+        self.player.state["my"] -= settings.gravity
+        if key_is_down(_states, pygame.K_SPACE):
+            if self.mob_on_ground(self.player):
+                self.player.state["my"] += 0.5
+        if key_is_down(_states, pygame.K_a):
+            self.player.state["mx"] = -data.mob_data["player"]["data"]["speed"]
+        if key_is_down(_states, pygame.K_d):
+            self.player.state["mx"] = data.mob_data["player"]["data"]["speed"]
+        self.player = self.move(self.player)
     def display(self, _x, _y) -> None:
         float_x = math.modf(_x)[0]
         float_y = math.modf(_y)[0]
@@ -150,11 +254,13 @@ def display(_x, _y):
 
 
 return_value = -1
+key_states = {}
 while return_value == -1:
     events = pygame.event.get()
-    world.tick(events)
-    print(world.player.state["x"], world.player.state["y"])
+    key_states = get_key_states(events, key_states)
+    world.tick(key_states)
+    print(world.player.state["x"], world.player.state["y"], world.player.state["mx"], world.player.state["my"])
     display(world.player.state["x"], world.player.state["y"])
-    pygame.time.delay(100)
+    pygame.time.delay(10)
 pygame.quit()
 sys.exit(return_value)
